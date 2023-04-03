3b13509de8bd6bdbaf34479a373fd78f620caaf8
JonahHoffman59 <jonahhoffman122@gmail.com>
2023-04-03 14:48:16 -0500

    create room class

--- roomadventure.py
+++ roomadventure.py
@@ -6,7 +6,44 @@ from tkinter import *
 
 
 class Room:
-    pass
+    """A roo has a name and a filepath that points to a .gif image"""
+
+    def __init__(self, name: str, image_filepath):
+        self.name = name
+        self.image = image_filepath
+        self.exits = {}
+        self.items = {}
+        self.grabs = []
+
+    def add_exit(self, label: str, room: 'Room'):
+        self.exits[label] = room
+
+    def add_items(self, label: str, desc: str):
+        self.items[label] = desc
+
+    def add_grabs(self, label: str):
+        self.grabs.append(label)
+
+    def del_grabs(self, label: str):
+        self.grabs.remove(label)
+
+    def __str__(self) -> str:
+        # create the base response
+        result = f"You are in {self.name}\n"
+
+        # append the items you see in the rom
+        result += "You see: "
+        for item in self.items.keys():
+            result += item + " "
+        result += "\n"
+        
+        # append the exits available in the room
+        result += "Exits: "
+        for exit in self.exits.keys():
+            result += exit + " "
+        result += "\n"
+
+        return result
 
 
 class Game:
